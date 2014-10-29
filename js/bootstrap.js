88f9209f0a7b70aa5113ff72684bb320a6b029c8
zahrakarim <zahra.karim@hotmail.com>
2014-10-29 16:02:08 -0400

    new page and minor changes

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -19,6 +19,28 @@ if (typeof jQuery === "undefined") { throw new Error("Bootstrap's JavaScript req
  * ======================================================================== */
 
 
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});
+
+
 +function ($) {
   'use strict';
 
